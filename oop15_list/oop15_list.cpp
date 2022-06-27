5bafd61c76a208d4e02331d69780d764cdf4956d
Zokerzes <romzes85@mail.ru>
2022-06-27 21:04:36 +0300

    Добавьте файлы проекта.

---
+++ oop15_list/oop15_list.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<iostream>
+#include"List.h"
+using namespace std;
+
+int main()
+{
+	//Создаем объект класса List
+	List lst;
+	
+	for (int i = 0; i < 10; i++)	lst.Add(i*2);
+
+	lst.Print();
+	
+	cout <<"elemrnt 8 - " << lst.findOfNumber(8);
+	cout << "elemrnt 3 - " << lst.findOfNumber(3);
+}
+
